ff6aeb88e5df9326c57c54385cdc7ecbcea56594
Pavel Yosifovich <zodiacon@live.com>
2021-10-21 20:46:17 +0300

    Add project files.

---
+++ WTLHelper/WTLHelper.cpp
@@ -0,0 +1,4 @@
+// WTLHelper.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
---
+++ WTLHelper/pch.h
@@ -0,0 +1,25 @@
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#pragma once
+
+#define __cpp_lib_ranges
+
+#include <Windows.h>
+#include <atlbase.h>
+#include <atlapp.h>
+#include <atlstr.h>
+#include <atlctrls.h>
+#include <atlctrlw.h>
+#include <atlctrlx.h>
+#include <atltypes.h>
+
+#include <vector>
+#include <memory>
+#include <ranges>
+#include <map>
+#include <unordered_map>
+
