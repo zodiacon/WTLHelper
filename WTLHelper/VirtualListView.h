27552088d52f06de3610c3589b72b8d4cefbd7eb
Pavel Yosifovich <zodiacon@live.com>
2022-02-22 15:48:33 +0200

    virtual list view improvements

--- WTLHelper/VirtualListView.h
+++ WTLHelper/VirtualListView.h
@@ -11,20 +11,23 @@ enum class ListViewRowCheck {
 template<typename T>
 struct CVirtualListView {
 	BEGIN_MSG_MAP(CVirtualListView)
+		NOTIFY_CODE_HANDLER(LVN_ODSTATECHANGED, OnStateChanged)
 		NOTIFY_CODE_HANDLER(LVN_COLUMNCLICK, OnColumnClick)
+		NOTIFY_CODE_HANDLER(LVN_ITEMCHANGED, OnItemStateChanged)
 		NOTIFY_CODE_HANDLER(LVN_ODFINDITEM, OnFindItem)
 		NOTIFY_CODE_HANDLER(LVN_GETDISPINFO, OnGetDispInfo)
+		NOTIFY_CODE_HANDLER(NM_CLICK, OnClick)
 		NOTIFY_CODE_HANDLER(NM_RCLICK, OnRightClick)
 		NOTIFY_CODE_HANDLER(NM_DBLCLK, OnDoubleClick)
-
-		//NOTIFY_CODE_HANDLER(LVN_ITEMCHANGED, OnItemChanged)
 		ALT_MSG_MAP(1)
 		REFLECTED_NOTIFY_CODE_HANDLER(LVN_GETDISPINFO, OnGetDispInfo)
 		REFLECTED_NOTIFY_CODE_HANDLER(LVN_COLUMNCLICK, OnColumnClick)
+		REFLECTED_NOTIFY_CODE_HANDLER(LVN_ODSTATECHANGED, OnStateChanged)
+		REFLECTED_NOTIFY_CODE_HANDLER(LVN_ITEMCHANGED, OnItemStateChanged)
 		REFLECTED_NOTIFY_CODE_HANDLER(LVN_ODFINDITEM, OnFindItem)
 		REFLECTED_NOTIFY_CODE_HANDLER(NM_RCLICK, OnRightClick)
+		REFLECTED_NOTIFY_CODE_HANDLER(NM_CLICK, OnClick)
 		REFLECTED_NOTIFY_CODE_HANDLER(NM_DBLCLK, OnDoubleClick)
-		//REFLECTED_NOTIFY_CODE_HANDLER(LVN_ITEMCHANGED, OnItemChanged)
 	END_MSG_MAP()
 
 	struct SortInfo {
@@ -65,6 +68,13 @@ struct CVirtualListView {
 		header.SetItem(si->RealSortColumn, &h);
 		si->SortColumn = -1;
 		return true;
+	}
+
+	LRESULT OnClick(int, LPNMHDR hdr, BOOL& handled) {
+		auto lv = (NMITEMACTIVATE*)hdr;
+		auto pT = static_cast<T*>(this);
+		pT->OnListViewClick(hdr->hwndFrom, lv->iItem, lv->iSubItem, lv->ptAction);
+		return 0;
 	}
 
 	LRESULT OnDoubleClick(int, LPNMHDR hdr, BOOL& handled) {
@@ -130,6 +140,9 @@ struct CVirtualListView {
 		return false;
 	}
 
+	void OnListViewClick(HWND, int row, int col, POINT const& pt) const {
+	}
+
 protected:
 	ColumnManager* GetExistingColumnManager(HWND hListView) const {
 		auto it = std::find_if(m_Columns.begin(), m_Columns.end(), [=](auto& cm) {
@@ -155,6 +168,20 @@ protected:
 		return cm ? cm->GetRealColumn(column) : column;
 	}
 
+	LRESULT OnStateChanged(int /*idCtrl*/, LPNMHDR hdr, BOOL& /*bHandled*/) {
+		auto lv = (NMLVODSTATECHANGE*)hdr;
+		auto p = static_cast<T*>(this);
+		p->OnStateChanged(hdr->hwndFrom, lv->iFrom, lv->iTo, lv->uOldState, lv->uNewState);
+		return 0;
+	}
+
+	LRESULT OnItemStateChanged(int /*idCtrl*/, LPNMHDR hdr, BOOL& /*bHandled*/) {
+		auto lv = (NMLISTVIEW*)hdr;
+		auto p = static_cast<T*>(this);
+		p->OnStateChanged(hdr->hwndFrom, lv->iItem, lv->iItem, lv->uOldState, lv->uNewState);
+		return 0;
+	}
+
 	LRESULT OnGetDispInfo(int /*idCtrl*/, LPNMHDR hdr, BOOL& /*bHandled*/) {
 		auto lv = (NMLVDISPINFO*)hdr;
 		auto& item = lv->item;
@@ -169,8 +196,11 @@ protected:
 		if (item.mask & LVIF_IMAGE) {
 			item.iImage = p->GetRowImage(hdr->hwndFrom, item.iItem, col);
 		}
+		if (item.mask & LVIF_PARAM) {
+			item.lParam = p->GetRowParam(hdr->hwndFrom, item.iItem);
+		}
 		if (item.mask & LVIF_INDENT)
-			item.iIndent = p->GetRowIndent(item.iItem);
+			item.iIndent = p->GetRowIndent(hdr->hwndFrom, item.iItem);
 		if ((ListView_GetExtendedListViewStyle(hdr->hwndFrom) & LVS_EX_CHECKBOXES) && item.iSubItem == 0 && (item.mask & LVIF_STATE)) {
 			item.state = INDEXTOSTATEIMAGEMASK((int)p->IsRowChecked(item.iItem));
 			item.stateMask = LVIS_STATEIMAGEMASK;
@@ -180,22 +210,16 @@ protected:
 				item.stateMask |= LVIS_SELECTED;
 			}
 		}
+		if (item.mask & LVIF_STATE)
+			item.state = p->GetListViewItemState(hdr->hwndFrom, item.iItem);
 		return 0;
 	}
-
-	int m_Selected = -1;
 
 	PCWSTR GetExistingColumnText(HWND hWnd, int row, int column) const {
 		return nullptr;
 	}
 
-	LRESULT OnItemChanged(int /*idCtrl*/, LPNMHDR hdr, BOOL& bHandled) {
-		auto lv = (NMLISTVIEW*)hdr;
-		if (lv->uChanged & LVIF_STATE) {
-			if (lv->uNewState & LVIS_SELECTED)
-				m_Selected = lv->iItem;
-		}
-		return 0;
+	void OnStateChanged(HWND, int from, int to, UINT oldState, UINT newState) const {
 	}
 
 	LRESULT OnFindItem(int /*idCtrl*/, LPNMHDR hdr, BOOL& /*bHandled*/) {
@@ -226,56 +250,20 @@ protected:
 		return -1;
 	}
 
-	void Sort(SortInfo const* si, bool ensureVisible = false) {
+	void Sort(SortInfo const* si) {
 		ATLASSERT(si);
+		auto p = static_cast<T*>(this);
+		p->PreSort(si->hWnd);
+		p->DoSort(si);
+		p->PostSort(si->hWnd);
+	}
 
-		CListViewCtrl list(si->hWnd);
-		auto header = list.GetHeader();
-
-		auto selected = list.GetSelectedIndex();
-		// comparing 4 columns should be enough...
-		int count = min(4, header.GetItemCount());
-		std::vector<CString> selectedText;
-		if (selected >= 0) {
-			//list.SetItemState(selected, LVIS_SELECTED, 0);
-			CString text;
-			for (int i = 0; i < count; i++) {
-				if (list.GetItemText(selected, i, text))
-					selectedText.push_back(text);
-			}
-		}
-		static_cast<T*>(this)->DoSort(si);
-		if (selected >= 0) {
-			selected = -1;
-			int start = -1, i, n;
-			CString text;
-			while ((n = list.FindItem(selectedText[0], false, false, start)) >= 0) {
-				for (i = 1; i < count; i++) {
-					if (list.GetItemText(n, i, text) && text != selectedText[i]) {
-						break;
-					}
-				}
-				if (i == count) {
-					selected = n;
-					break;
-				}
-				start = n;
-			}
-
-		}
-		if (selected >= 0) {
-			if (ensureVisible) {
-				list.EnsureVisible(selected, FALSE);
-				//list.SetSelectionMark(selected);
-			}
-			list.SetItemState(selected, LVIS_SELECTED | LVIS_FOCUSED, LVIS_SELECTED | LVIS_FOCUSED);
-		}
-		else if (!selectedText.empty()) {
-			list.SetItemState(-1, 0, LVIS_SELECTED);
-		}
+	void IsSorting(bool sorting) {
+		m_IsSorting = sorting;
 	}
 
 	LRESULT OnColumnClick(int /*idCtrl*/, LPNMHDR hdr, BOOL& /*bHandled*/) {
+		IsSorting(true);
 		auto lv = (NMLISTVIEW*)hdr;
 		auto col = GetRealColumn(hdr->hwndFrom, lv->iSubItem);
 
@@ -325,6 +313,7 @@ protected:
 		//}
 
 		Sort(si);
+		IsSorting(false);
 		list.RedrawItems(list.GetTopIndex(), list.GetTopIndex() + list.GetCountPerPage());
 
 		return 0;
@@ -340,9 +329,16 @@ protected:
 			list.RedrawItems(list.GetTopIndex(), list.GetTopIndex() + list.GetCountPerPage());
 	}
 
+	bool IsSorting() const {
+		return m_IsSorting;
+	}
+
 	bool IsSortable(HWND, int) const {
 		return true;
 	}
+
+	void PostSort(HWND) const {}
+	void PreSort(HWND) const {}
 
 	int GetSortColumn(HWND hWnd, UINT_PTR id = 0) const {
 		auto si = FindById(id);
@@ -370,11 +366,19 @@ protected:
 		return L"";
 	}
 
+	LPARAM GetRowParam(HWND, int row) const {
+		return 0;
+	}
+
 	int GetRowImage(HWND hWnd, int row, int col) const {
 		return -1;
 	}
 
-	int GetRowIndent(int row) const {
+	int GetRowIndent(HWND, int row) const {
+		return 0;
+	}
+
+	DWORD GetListViewItemState(HWND, int row) const {
 		return 0;
 	}
 
@@ -403,4 +407,6 @@ private:
 
 	mutable std::vector<SortInfo> m_Controls;
 	mutable std::vector<std::unique_ptr<ColumnManager>> m_Columns;
+	int m_Selected = -1;
+	bool m_IsSorting{ false };
 };
