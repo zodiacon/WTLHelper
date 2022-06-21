ba6a7a05fd001353afcc4bc7d0283ca5bd8a75a5
Pavel Yosifovich <zodiacon@live.com>
2022-06-21 16:57:14 +0300

    fixed dialog back color

--- CustomDialog.h
+++ CustomDialog.h
@@ -6,27 +6,31 @@ class CCustomDialog : public CWindowImpl<CCustomDialog> {
 public:
 	BEGIN_MSG_MAP(CCustomDialog)
 		MESSAGE_HANDLER(WM_CTLCOLORSTATIC, OnStaticColor)
-		MESSAGE_HANDLER(WM_CTLCOLORDLG, OnStaticColor)
+		MESSAGE_HANDLER(WM_CTLCOLORDLG, OnDialogColor)
 		MESSAGE_HANDLER(WM_CTLCOLORLISTBOX, OnStaticColor)
 		MESSAGE_HANDLER(WM_CTLCOLORSCROLLBAR, OnScrollBarColor)
 		MESSAGE_HANDLER(WM_CTLCOLOREDIT, OnEditColor)
 	END_MSG_MAP()
-
-	void Init() {
-	}
 
 	LRESULT OnScrollBarColor(UINT, WPARAM wp, LPARAM lp, BOOL&) {
 		return (LRESULT)::GetSysColorBrush(COLOR_WINDOW);
 	}
 
 	LRESULT OnDialogColor(UINT, WPARAM wp, LPARAM lp, BOOL&) {
-		if (ThemeHelper::IsDefault())
-			return DefWindowProc();
+		auto theme = ThemeHelper::GetCurrentTheme();
+		ATLASSERT(theme);
+
+		CDCHandle dc((HDC)wp);
+		dc.SetBkMode(OPAQUE);
+		dc.SetTextColor(theme->TextColor);
+		dc.SetBkColor(theme->BackColor);
 		return (LRESULT)::GetSysColorBrush(COLOR_WINDOW);
 	}
 
 	LRESULT OnStaticColor(UINT, WPARAM wp, LPARAM lp, BOOL& handled) {
 		auto theme = ThemeHelper::GetCurrentTheme();
+		ATLASSERT(theme);
+
 		CDCHandle dc((HDC)wp);
 		dc.SetBkMode(OPAQUE);
 		dc.SetTextColor(theme->TextColor);
@@ -36,6 +40,8 @@ public:
 
 	LRESULT OnEditColor(UINT, WPARAM wp, LPARAM lp, BOOL&) {
 		auto theme = ThemeHelper::GetCurrentTheme();
+		ATLASSERT(theme);
+
 		CDCHandle dc((HDC)wp);
 		dc.SetBkMode(OPAQUE);
 		dc.SetTextColor(theme->TextColor);
