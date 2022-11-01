4c76ea7122c5e88f514402b54878d8ca1753f940
Pavel Yosifovich <zodiacon@live.com>
2022-11-01 11:26:23 -0400

    macro for routing WM_COMMAND messages to active tab

--- CustomTabView.h
+++ CustomTabView.h
@@ -12,3 +12,15 @@ public:
 
 	bool m_Redraw{ true };
 };
+
+#define COMMAND_TABVIEW_HANDLER(tabs, msgMapId)	\
+	if(uMsg == WM_COMMAND) {	\
+		int page = tabs.GetActivePage();	\
+		if(page >= 0) {		\
+			auto map = (CMessageMap*)tabs.GetPageData(page);	\
+			ATLASSERT(map);		\
+			LRESULT result;		\
+			if(map->ProcessWindowMessage(m_hWnd, uMsg, wParam, lParam, result, msgMapId))	\
+				return TRUE;	\
+		}		\
+	}
