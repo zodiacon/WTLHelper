1bd83f5b757e2532a65997b4a1c2b58c45650bb2
Pavel Yosifovich <zodiacon@live.com>
2026-02-24 19:45:53 -0500

    native dark mode support

--- WTLHelper/WTLHelper.cpp
+++ WTLHelper/WTLHelper.cpp
@@ -2,3 +2,42 @@
 //
 
 #include "pch.h"
+#include "WTLHelper.h"
+#include "DarkMode/DarkModeSubclass.h"
+
+static LRESULT OnHook(int code, WPARAM wp, LPARAM lp) {
+	if (code == HC_ACTION) {
+		auto msg = (CWPRETSTRUCT*)lp;
+		if (msg->message == WM_INITDIALOG) {
+			DarkMode::setDarkWndNotifySafe(msg->hwnd);
+		}
+
+		else if (msg->message == WM_CREATE) {
+			auto hwnd = msg->hwnd;
+			auto lpcs = (LPCREATESTRUCT)msg->lParam;
+			if (lpcs->style & WS_CHILD) {
+				DarkMode::setWindowExStyle(msg->hwnd, true, WS_EX_COMPOSITED);
+				DarkMode::setWindowEraseBgSubclass(msg->hwnd);
+			}
+			else {
+				// top-level window
+				DarkMode::setDarkWndNotifySafe(msg->hwnd);
+				DarkMode::setWindowEraseBgSubclass(msg->hwnd);
+				DarkMode::setWindowMenuBarSubclass(msg->hwnd);
+				//DarkMode::setWindowExStyle(msg->hwnd, true, WS_EX_COMPOSITED);
+				::SetWindowLongPtr(hwnd, GWL_EXSTYLE, GetWindowLongPtr(hwnd, GWL_EXSTYLE) | WS_EX_COMPOSITED);
+			}
+		}
+	}
+	return ::CallNextHookEx(nullptr, code, wp, lp);
+}
+
+bool WTLHelper::InitDarkMode() {
+	DarkMode::initDarkMode();
+	DarkMode::setDarkModeConfigEx(static_cast<UINT>(DarkMode::DarkModeType::dark));
+	DarkMode::setDefaultColors(true);
+	DarkMode::setColorizeTitleBarConfig(false);
+
+	::SetWindowsHookEx(WH_CALLWNDPROCRET, OnHook, nullptr, GetCurrentThreadId());
+	return true;
+}
